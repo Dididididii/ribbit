5bd14b90a3e3a731cf1a01a3fb61d2c9dd75900d
Dididididii <1281627297@qq.com>
2022-04-01 14:46:55 +0800

    请求组件更新

---
+++ eribbit-client-pc-124/src/store/modules/user.js
@@ -0,0 +1,20 @@
+
+export default {
+  state () {
+    return {
+      profile: {
+        id: '',
+        avatar: '',
+        nickname: '',
+        account: '',
+        mobile: '',
+        token: ''
+      }
+    }
+  },
+  mutations: {
+    setUser (state, payload) {
+      state.profile = payload
+    }
+  }
+}
