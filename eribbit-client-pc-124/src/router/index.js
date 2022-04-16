e400c47459d8126082a2a285eab1fd41f6efe28d
Dididididii <1281627297@qq.com>
2022-04-16 11:43:27 +0800

    登录页面

--- eribbit-client-pc-124/src/router/index.js
+++ eribbit-client-pc-124/src/router/index.js
@@ -5,6 +5,7 @@ import SubCategory from '@/views/category/sub'
 const Layout = () => import('@/views/Layout')
 const Home = () => import('@/views/home')
 const Goods = () => import('@/views/goods/index')
+const Login = () => import('@/views/login/index')
 
 const routes = [
   {
@@ -16,7 +17,8 @@ const routes = [
       { path: '/category/sub/:id', component: SubCategory },
       { path: '/product/:id', component: Goods }
     ]
-  }
+  },
+  { path: '/login', component: Login }
 ]
 
 const router = createRouter({
